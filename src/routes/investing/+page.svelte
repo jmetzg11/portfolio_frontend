0ccd1be8e1ab1f97cca8b50944711f45d0d9e997
jmetzg11alpine <jmetzg11@gmail.com>
2024-12-05 17:19:21 -0800

    started portfolio

---
+++ src/routes/investing/+page.svelte
@@ -0,0 +1,1 @@
+<h1>investing stuff</h1>
--- src/routes/web-assembly/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import Grid from './Grid.svelte';
-</script>
-
-<div class="flex flex-col items-center justify-center h-screen"><Grid /></div>
-
-<style>
-</style>
